94e2dc1a00e106bb267a610e52338a119a19ae9d
Darren Howley <d.howley77@gmail.com>
2020-12-02 12:58:36 +0000

    Add the custom tab to the creative menu, Added first item... Ruby item

--- src/main/java/com/evilspawn/tutorial/Tutorial.java
+++ src/main/java/com/evilspawn/tutorial/Tutorial.java
@@ -1,5 +1,8 @@
 package com.evilspawn.tutorial;
 
+import com.evilspawn.tutorial.init.ModItems;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -12,14 +15,24 @@ import org.apache.logging.log4j.Logger;
 public class Tutorial
 {
     public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "tutorial";
 
     public Tutorial() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        ModItems.init();
 
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event){}
     private void doClientStuff(final FMLClientSetupEvent event) {}
+
+    public static final ItemGroup TAB = new ItemGroup("tutorialTab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(ModItems.RUBY.get());
+        }
+    };
 }
---
+++ src/main/java/com/evilspawn/tutorial/init/ModItems.java
@@ -0,0 +1,21 @@
+package com.evilspawn.tutorial.init;
+
+import com.evilspawn.tutorial.Tutorial;
+import com.evilspawn.tutorial.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Tutorial.MOD_ID);
+
+    public static void init() {
+        Tutorial.LOGGER.info("Loading items!");
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+}
